2a4b2a35d182f511e610968b764cbefdb553b7ef
Sudam-Praveen <sudam.peramuna@gmail.com>
2024-01-18 20:47:57 +0530

    first commit

---
+++ src/main/java/org/example/service/Impl/BookServiceImpl.java
@@ -0,0 +1,12 @@
+package org.example.service.Impl;
+
+import org.example.dto.Book;
+import org.example.service.BookService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl implements BookService {
+    public void addBook(Book book){
+
+    }
+}
